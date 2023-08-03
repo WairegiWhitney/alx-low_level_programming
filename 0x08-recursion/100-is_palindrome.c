6580ed8b348f21cf2dab7b986e4ecf4285c59702
Wairegi Whitney <wairegiwhitney11@gmail.com>
2023-08-03 15:00:19 -0700

    0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,57 @@
 #include "main.h"
 
 /**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not
- *
- * @s: string
- * Return: int
- */
+* is_palindrome - returns true if the given string is a palindrome
+*@s: string to check
+*Return: true if the given string is a palindrome
+*/
+
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s), i = 0;
+	int index = 0;
+	int len = find_strlen(s);
 
-	if (len == 0)
+	if (!(*s))
 		return (1);
-	return (palindrome_check(s, len - 1, i));
+
+	return (check_palindrome(s, len, index));
 }
 
 /**
- * _strlen_recursion - returns the length of a string
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * @s: string
- * Return: int
+ * Return: The length of the string.
  */
-int _strlen_recursion(char *s)
+int find_strlen(char *s)
 {
-	int len;
+	int len = 0;
 
-	if (*s == '\0')
-		return (0);
-	len = _strlen_recursion(s + 1);
-	return (len + 1);
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
 }
 
 /**
- * palindrome_check - returns 1 if palindrome and 0 if not
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
  *
- * @s: string
- * @len: length of string
- * @i: counter variable
- * Return: int
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
-int palindrome_check(char *s, int len, int i)
+int check_palindrome(char *s, int len, int index)
 {
-	if (len == i)
+	if (s[index] == s[len / 2])
 		return (1);
-	else if (s[len] != s[i])
-		return (0);
-	if (i < len)
-		return (palindrome_check(s, --len, ++i));
-	return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
 }
