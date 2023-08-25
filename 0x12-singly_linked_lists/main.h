e51d085d7aa92e4c302d78c382662310cad65b45
Wairegi Whitney <wairegiwhitney11@gmail.com>
2023-08-25 08:58:23 -0700

    data structures

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <stddef.h>
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
