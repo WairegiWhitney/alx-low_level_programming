842f401adf3c1ff37fc11d6e3263e458bf3f7ebb
Wairegi Whitney <wairegiwhitney11@gmail.com>
2023-08-25 09:36:09 -0700

    data structures

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,22 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 
 
 #include <stddef.h>
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-int _putchar(char c);
-
 typedef struct list_w
 {
     char *str;
     struct list_w *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+
+
 
 #endif
