0ad1842f5918cea7018ba69679baa0759ebe89f7
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2023-08-30 19:51:21 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,8 +45,3 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
        temp->next=new;
        return new;
 }
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
