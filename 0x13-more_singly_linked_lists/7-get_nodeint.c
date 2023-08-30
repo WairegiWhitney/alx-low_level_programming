b140b43049aa5eea524ddb198f731e5f20d4f17f
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2023-08-30 19:41:11 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,8 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int n;
-    for(n=0;n<index;n++)
+    unsigned int element;
+    for(element=0;element<index;element++)
     {
         if(head=NULL)
         {
