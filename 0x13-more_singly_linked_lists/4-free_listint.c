f76bf6005aab330cb47fe6b6a1c43a77ffdd2165
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2023-08-30 19:14:30 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,7 +5,7 @@
  * free_listint - the function Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list to be freed.
  */
-Prototype: void free_listint(listint_t *head)
+void free_listint(listint_t *head)
 {
   listint_t* temp = head;
   listint_t* next;
