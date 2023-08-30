aa7ce49b0d21131b17139bf99e4d7e9dea736a04
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2023-08-30 19:53:44 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
    listint_t* new=(listint_t*)malloc(sizeof(listint_t));
+   unsigned int i;
    if(new==NULL)
    {
        return NULL;
