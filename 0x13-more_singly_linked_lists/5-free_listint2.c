11aedcbd1e048d3f184e3133a528ed0975f0450c
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2023-08-30 19:27:14 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,17 @@
  */
 void free_listint2(listint_t **head)
 {
-  listint_t* temp = head;
-  listint_t* next;
-  while(temp!=NULL)
-  {
-      next=temp->next;
-      free (temp);
-      temp=next;
-  }
-  *head=NULL;
+	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
 }
